c2b9ba2c73dc9275eb1b916ee1f691e59c9b1f57
amit98765 <amitkumarmittalakm@gmail.com>
2017-09-27 16:17:12 +1000

    nothing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Component2Component } from './component2/component2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Component2Component
   ],
   imports: [
     BrowserModule
---
+++ src/app/component2/component2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nested',
+  templateUrl: './component2.component.html',
+  styleUrls: ['./component2.component.css']
+})
+export class Component2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
